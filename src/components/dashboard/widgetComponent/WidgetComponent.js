0ffc87ce6eb8cd69553f6b100a5df02c3884b894
Andy Nguyen <nguyenanhtu2204tb@gmail.com>
2023-10-01 22:51:59 +0700

    update

--- src/components/dashboard/widgetComponent/WidgetComponent.js
+++ src/components/dashboard/widgetComponent/WidgetComponent.js
@@ -70,6 +70,11 @@ const renderData = useMemo(()=>{
 },[dataLeaguesStandings])
   return (
     <div>
+      <div className='border rounded p-4 mb-4 mt-4'>
+        <div className='flex items-center gap-4'><span className='text-red text-xl'><AiFillCheckCircle/></span> <span>Win</span></div>
+        <div className='flex items-center gap-4 mt-4'><span className='text-gray text-xl'><AiFillMinusCircle/></span> <span>Draw</span></div>
+        <div className='flex items-center gap-4 mt-4'><span className='text-red text-xl'><AiFillCloseCircle/></span> <span>Lose</span></div>
+      </div>
        {loading ? <Loading/> :renderData} 
     </div>
   );
